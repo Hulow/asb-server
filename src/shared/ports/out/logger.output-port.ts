614d4662553b1f054bc318eb6c3707f178bc03ed
vicc leff <hlw@viccs-MBP.fritz.box>
2023-05-08 10:42:02 +0200

    feat: definer logger port

---
+++ src/shared/ports/out/logger.output-port.ts
@@ -0,0 +1,24 @@
+export enum LogLevel {
+  Debug = 'debug',
+  Error = 'error',
+  Info = 'info',
+  Warn = 'warn',
+}
+
+export interface LogMethod {
+  (msg: string, meta: object): void;
+  (obj: unknown): void;
+}
+
+export interface LoggerConfig {
+  readonly level: LogLevel;
+}
+
+export const LOGGER_OUTPUT_PORT = Symbol.for('LoggerOutputPort');
+
+export interface LoggerOutputPort {
+  debug: LogMethod;
+  info: LogMethod;
+  warn: LogMethod;
+  error: LogMethod;
+}
