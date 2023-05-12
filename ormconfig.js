96277f8182419cd7b744deea93df6c20f83380b5
vicc leff <hlw@viccs-MacBook-Pro.local>
2023-05-12 20:26:18 +0200

    feat: setup orm config

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { DataSource } = require('typeorm');
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_DATABASE_HOSTNAME || 'localhost',
+  port: +(process.env.POSTGRES_DATABASE_PORT || 5432),
+  username: process.env.POSTGRES_DATABASE_USER || 'docker',
+  password: process.env.POSTGRES_DATABASE_PASSWORD || 'docker',
+  database: process.env.POSTGRES_DATABASE_NAME || 'asb',
+  entities: [`src/*/adapters/out/persistence/*.orm-entity.ts`],
+  migrations: [`migrations/*.ts`],
+  migrationsTransactionMode: 'all',
+});
+
+module.exports = { dataSource };
