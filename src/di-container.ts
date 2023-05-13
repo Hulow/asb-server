6051f6bf0f47bc24d3129694f86255cef3baf0ee
vicc leff <hlw@viccs-MacBook-Pro.local>
2023-05-13 16:18:48 +0200

    feat: bind pg datasource to container

--- src/di-container.ts
+++ src/di-container.ts
@@ -1,8 +1,15 @@
 import { Container } from 'inversify';
 
 import { config } from './config';
+import { PostgresDataSource } from './shared/adapters/out/postgres-datasource';
 import { PinoLogger } from './shared/adapters/out/pino-logger';
 import { LoggerOutputPort, LOGGER_OUTPUT_PORT } from './shared/ports/out/logger.output-port';
+import { SqlDriverRepository } from './driver/adapters/out/persistence/driver.repository.sql';
+
+import {
+  DriverRepositoryOutputPort,
+  DRIVER_REPOSITORY_OUTPUT_PORT,
+} from './driver/core/application/ports/out/driver-repository.output-port';
 
 export const container = new Container({
   autoBindInjectable: true,
@@ -14,3 +21,7 @@ export const container = new Container({
  *  output/driven/secondary adapters
  */
 container.bind<LoggerOutputPort>(LOGGER_OUTPUT_PORT).toDynamicValue(() => new PinoLogger(config.logger));
+container
+  .bind(PostgresDataSource)
+  .toDynamicValue(() => new PostgresDataSource(config.postgres, container.get(LOGGER_OUTPUT_PORT)));
+container.bind<DriverRepositoryOutputPort>(DRIVER_REPOSITORY_OUTPUT_PORT).to(SqlDriverRepository);
