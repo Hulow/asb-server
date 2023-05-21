afed97fbef44a678790c689c0bc674855a13ce10
vicc leff <hlw@viccs-MBP.fritz.box>
2023-05-21 04:30:16 +0200

    chore: update migration file with cabinet

--- migrations/1683906159011-init.ts
+++ migrations/1683906159011-init.ts
@@ -5,9 +5,13 @@ export class init1683906159011 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "driver" ("uid" uuid NOT NULL, "brand_name" varchar NOT NULL, "product_name" varchar NOT NULL, "driver_type" varchar NOT NULL, "nominal_diameter" float NOT NULL, "nominal_impedance" float NOT NULL, "continuous_power_handling" float NOT NULL, "user_id" integer NOT NULL, "cabinet_id" integer NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_df955cae05f17b2bcf5045cc021" PRIMARY KEY ("uid"))`,
     );
+    await queryRunner.query(
+      `CREATE TABLE "cabinet" ("uid" uuid NOT NULL, "brand_name" varchar NOT NULL, "product_name" varchar NOT NULL, "enclosure_type" varchar NOT NULL, "weight" float NOT NULL, "dimension" varchar NOT NULL, "manufacturing_year" integer NOT NULL, "description" varchar NOT NULL, "user_id" integer NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_2f25fae55a3bd80337501b310e3" PRIMARY KEY ("uid"))`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE "driver"`);
+    await queryRunner.query(`DROP TABLE "cabinet"`);
   }
 }
