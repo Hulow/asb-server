d30a81b8d4a7feb471bf2151707fbc7110dcae86
vicc leff <hlw@viccs-MacBook-Pro.local>
2023-05-21 06:10:47 +0200

    feat: add user table

--- migrations/1683906159011-init.ts
+++ migrations/1683906159011-init.ts
@@ -8,10 +8,14 @@ export class init1683906159011 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "cabinet" ("uid" uuid NOT NULL, "brand_name" varchar NOT NULL, "product_name" varchar NOT NULL, "enclosure_type" varchar NOT NULL, "weight" float NOT NULL, "dimension" varchar NOT NULL, "manufacturing_year" integer NOT NULL, "description" varchar NOT NULL, "user_id" integer NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_2f25fae55a3bd80337501b310e3" PRIMARY KEY ("uid"))`,
     );
+    await queryRunner.query(
+      `CREATE TABLE "user" ("uid" uuid NOT NULL, "first_name" varchar NOT NULL, "last_name" varchar NOT NULL, "username" varchar NOT NULL, "email" float NOT NULL, "phone_number" varchar NOT NULL, "city" integer NOT NULL, "description" varchar NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_4b8cadc8a1d7af2b417fd5ca59f" PRIMARY KEY ("uid"))`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE "driver"`);
     await queryRunner.query(`DROP TABLE "cabinet"`);
+    await queryRunner.query(`DROP TABLE "user"`);
   }
 }
