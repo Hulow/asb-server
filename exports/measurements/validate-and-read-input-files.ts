cfa2a5a3b34218ca5cbfb1e1f629f36a4555388e
vicc leff <hlw@viccs-MBP.fritz.box>
2023-05-24 23:10:20 +0200

    chore: remove empty class

--- exports/measurements/validate-and-read-input-files.ts
+++
@@ -1,7 +0,0 @@
-export class ValidateAndReadInputFiles {
-  // split into 2 flow: JSON and TXT
-  //READ EACH FILE AND GET THE DATA
-  //TXT: check if it's string, not empty
-  //JSON: IF each field is not empty
-  // RETURN THE DATA
-}
