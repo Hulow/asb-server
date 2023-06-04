8583ff60ef486ada015d48c0706ef4f1258cb97a
vicc leff <hlw@viccs-MBP.fritz.box>
2023-06-04 18:41:17 +0200

    feat: improve axios middleware

--- src/shared/adapters/in/axios-client.ts
+++ src/shared/adapters/in/axios-client.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
+import zlib from 'zlib';
 
 import { LoggerOutputPort } from '../../ports/out/logger.output-port';
 
@@ -11,11 +12,27 @@ export class AxiosClient {
   protected readonly instance: AxiosInstance;
   constructor(private readonly _config: AxiosConfig, private _logger: LoggerOutputPort) {
     this.instance = axios.create({
-      baseURL: _config.asbBaseUrl,
+      baseURL: this._config.asbBaseUrl,
       headers: {
-        Authorization: _config.asbKeyUrl,
-        'content-type': 'application/gzip',
+        Authorization: this._config.asbKeyUrl,
+        'content-type': 'application/zlib',
       },
+      transformRequest: [
+        (data: object) => {
+          return this.encryptData(this.compressData(data));
+        },
+      ],
+      withCredentials: true,
+      responseType: 'json',
+      timeout: 1000,
     });
+  }
+
+  private compressData(data: object) {
+    return zlib.gzipSync(JSON.stringify(data));
+  }
+
+  private encryptData(data: Buffer) {
+    return data.toString('base64');
   }
 }
