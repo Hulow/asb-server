6ecefdf60d1f0bb3b2417500cf98dd9b539661f9
vicc leff <hlw@viccs-MBP.local>
2023-06-21 21:24:12 +0200

    feat: add cabinet registration service

--- src/cabinet/core/application/ports/out/cabinet-repository.output-port.ts
+++ src/cabinet/core/application/ports/out/cabinet-repository.output-port.ts
@@ -1,7 +1,9 @@
 import { Cabinet } from '../../../domain/cabinet';
+import { Owner } from '../../../../../owner/core/domain/owner';
 
 export interface CabinetRepositoryOutputPort {
-  save: (driver: Cabinet) => Promise<Cabinet>;
+  save: (cabinet: Cabinet, OwnerUid: Owner) => Promise<Cabinet>;
+  getByProductNameAndOwnerUid: (productName: string, ownerUid: string) => Promise<Cabinet | undefined>;
 }
 
 export const CABINET_REPOSITORY_OUTPUT_PORT = Symbol.for('CabinetRepositoryOutputPort');
--- src/owner/core/application/ports/out/owner-repository.output-port.ts
+++ src/owner/core/application/ports/out/owner-repository.output-port.ts
@@ -3,6 +3,7 @@ import { Owner } from '../../../domain/owner';
 export interface OwnerRepositoryOutputPort {
   save: (owner: Owner) => Promise<Owner>;
   getByOwnername: (ownername: string) => Promise<Owner | undefined>;
+  getById: (ownerUid: string) => Promise<Owner | undefined>;
 }
 
 export const OWNER_REPOSITORY_OUTPUT_PORT = Symbol.for('OwnerRepositoryOutputPort');
