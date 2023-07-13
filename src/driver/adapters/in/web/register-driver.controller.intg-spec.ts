09572def2c0a1669b9fb9b20c17b516b9bdd4af3
vicc leff <hlw@viccs-MBP.fritz.box>
2023-07-13 20:14:39 +0200

    fix: test intg

--- src/driver/adapters/in/web/register-driver.controller.intg-spec.ts
+++ src/driver/adapters/in/web/register-driver.controller.intg-spec.ts
@@ -24,6 +24,53 @@ const cabinetRepository = container.get<CabinetRepositoryOutputPort>(CABINET_REP
 const ownerRepository = container.get<OwnerRepositoryOutputPort>(OWNER_REPOSITORY_OUTPUT_PORT);
 const driverRepository = container.get<DriverRepositoryOutputPort>(DRIVER_REPOSITORY_OUTPUT_PORT);
 
+function createOwner(ownerUid: string) {
+  return {
+    uid: ownerUid,
+    firstName: 'firstName',
+    lastName: 'lastName',
+    ownername: 'ownername',
+    email: 'email',
+    phoneNumber: 'phoneNumber',
+    city: 'city',
+    description: 'description',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+}
+
+function createCabinet(cabinetUid: string, ownerUid: string) {
+  return {
+    uid: cabinetUid,
+    brandName: 'Clauz',
+    productName: 'die Maschine',
+    enclosureType: 'Poccochin',
+    weight: 100,
+    dimension: 'dimension',
+    manufacturingYear: 2023,
+    description: 'description',
+    ownerUid: ownerUid,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+}
+
+function createDriver(driverUid: string, cabinetUid: string) {
+  return {
+    uid: driverUid,
+    brandName: 'B&C',
+    productName: '12PE32',
+    driverType: 'Woofer',
+    manufacturingYear: 2015,
+    nominalDiameter: 12,
+    nominalImpedance: 8,
+    continuousPowerHandling: 500,
+    cabinetUid: cabinetUid,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+}
+
 describe(`/api/driver/register`, () => {
   beforeAll(async () => {
     await database.start();
@@ -38,32 +85,12 @@ describe(`/api/driver/register`, () => {
   });
 
   it(`register a driver`, async () => {
-    const existingOwner = {
-      uid: '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
-      firstName: 'firstName',
-      lastName: 'lastName',
-      ownername: 'ownername',
-      email: 'email',
-      phoneNumber: 'phoneNumber',
-      city: 'city',
-      description: 'description',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    const existingOwner = createOwner('4343b2ab-a22e-4d12-ac13-6bb399d4e512');
     await ownerRepository.save(existingOwner);
-    const existingCabinet = {
-      uid: '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
-      brandName: 'Clauz',
-      productName: 'die Maschine',
-      enclosureType: 'Poccochin',
-      weight: 100,
-      dimension: 'dimension',
-      manufacturingYear: 2023,
-      description: 'description',
-      ownerUid: '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    const existingCabinet = createCabinet(
+      '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
+      '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
+    );
     await cabinetRepository.save(existingCabinet);
 
     const registerDriverInput = {
@@ -78,19 +105,21 @@ describe(`/api/driver/register`, () => {
       ownerUid: '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
     };
 
-    const expectedResponse = {
-      uid: expect.stringMatching(UUID_V4_REGEX) as string,
-      createdAt: expect.any(Date) as Date,
-      updatedAt: expect.any(Date) as Date,
-      brandName: 'B&C',
-      productName: '12PE32',
-      driverType: 'Woofer',
-      manufacturingYear: 2015,
-      nominalDiameter: 12,
-      nominalImpedance: 8,
-      continuousPowerHandling: 500,
-      cabinetUid: '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
-    };
+    const expectedResponse = [
+      {
+        uid: expect.stringMatching(UUID_V4_REGEX) as string,
+        createdAt: expect.any(Date) as Date,
+        updatedAt: expect.any(Date) as Date,
+        brandName: 'B&C',
+        productName: '12PE32',
+        driverType: 'Woofer',
+        manufacturingYear: 2015,
+        nominalDiameter: 12,
+        nominalImpedance: 8,
+        continuousPowerHandling: 500,
+        cabinetUid: '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
+      },
+    ];
 
     const response: { body: { cabinetUid: string; productName: string } } = await request(expressApp)
       .post('/api/driver/register')
@@ -102,32 +131,12 @@ describe(`/api/driver/register`, () => {
     ).toEqual(expectedResponse);
   });
   it(`does not register a driver without owner`, async () => {
-    const existingOwner = {
-      uid: '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
-      firstName: 'firstName',
-      lastName: 'lastName',
-      ownername: 'ownername',
-      email: 'email',
-      phoneNumber: 'phoneNumber',
-      city: 'city',
-      description: 'description',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    const existingOwner = createOwner('4343b2ab-a22e-4d12-ac13-6bb399d4e512');
     await ownerRepository.save(existingOwner);
-    const existingCabinet = {
-      uid: '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
-      brandName: 'Clauz',
-      productName: 'die Maschine',
-      enclosureType: 'Poccochin',
-      weight: 100,
-      dimension: 'dimension',
-      manufacturingYear: 2023,
-      description: 'description',
-      ownerUid: '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    const existingCabinet = createCabinet(
+      '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
+      '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
+    );
     await cabinetRepository.save(existingCabinet);
 
     const registerDriverInput = {
@@ -148,49 +157,21 @@ describe(`/api/driver/register`, () => {
       .set({ Authorization: config.express.asbKeyUrl, Accept: 'application/json' })
       .expect(500);
   });
-  it(`does not register an existing driver`, async () => {
-    const existingOwner = {
-      uid: '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
-      firstName: 'firstName',
-      lastName: 'lastName',
-      ownername: 'ownername',
-      email: 'email',
-      phoneNumber: 'phoneNumber',
-      city: 'city',
-      description: 'description',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+  it(`does not register a driver already mounted 2 times on a box`, async () => {
+    const existingOwner = createOwner('4343b2ab-a22e-4d12-ac13-6bb399d4e512');
     await ownerRepository.save(existingOwner);
-    const existingCabinet = {
-      uid: '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
-      brandName: 'Clauz',
-      productName: 'die Maschine',
-      enclosureType: 'Poccochin',
-      weight: 100,
-      dimension: 'dimension',
-      manufacturingYear: 2023,
-      description: 'description',
-      ownerUid: '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    const existingCabinet = createCabinet(
+      '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
+      '4343b2ab-a22e-4d12-ac13-6bb399d4e512',
+    );
     await cabinetRepository.save(existingCabinet);
 
-    const existingDriver = {
-      uid: '4343b2ab-a22e-4d12-ac13-6bb399d4e515',
-      brandName: 'B&C',
-      productName: '12PE32',
-      driverType: 'Woofer',
-      manufacturingYear: 2015,
-      nominalDiameter: 12,
-      nominalImpedance: 8,
-      continuousPowerHandling: 500,
-      cabinetUid: '4343b2ab-a22e-4d12-ac13-6bb399d4e513',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    await driverRepository.save(existingDriver);
+    const firstDriver = createDriver('4343b2ab-a22e-4d12-ac13-6bb399d4e515', '4343b2ab-a22e-4d12-ac13-6bb399d4e513');
+    const secondDriver = createDriver('4343b2ab-a22e-4d12-ac13-6bb399d4e516', '4343b2ab-a22e-4d12-ac13-6bb399d4e513');
+    const thirdDriver = createDriver('4343b2ab-a22e-4d12-ac13-6bb399d4e517', '4343b2ab-a22e-4d12-ac13-6bb399d4e513');
+    for (const driver of [firstDriver, secondDriver, thirdDriver]) {
+      await driverRepository.save(driver);
+    }
 
     const registerDriverInput = {
       brandName: 'B&C',
